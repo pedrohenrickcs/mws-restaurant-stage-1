ff258a29ea78f487bebe594eadf75397d1453b2c
unknown <pedro.henrickcs@gmail.com>
2019-02-02 14:29:51 -0200

    review

--- js/aria.js
+++ js/aria.js
@@ -1,46 +1,34 @@
-const navigation = document.querySelector('header nav');
 const mapRestaurant = document.querySelector('#map');
 const filter = document.querySelector('.filter-options');
 const neighborhoodsFilter = document.querySelector('#neighborhoods-select');
 const cuisinesSelect = document.querySelector('#cuisines-select');
 const restaurantsList = document.querySelector('#restaurants-list');
 
-
-// Navigation
-if (navigation) {
-    navigation.setAttribute('role', 'navigation');
-    navigation.setAttribute('aria-label', 'navigation');
-}
-
 // Attribute of map
 if (mapRestaurant) {
-    mapRestaurant.setAttribute('role', 'map');
+    mapRestaurant.setAttribute('role', 'application');
     mapRestaurant.setAttribute('aria-label', 'map');
 }
 
 // Attribute of filter
 if (filter) {
-    filter.setAttribute('role', 'filter');
+    filter.setAttribute('role', 'listbox');
 }
 
 // Attribute of neighborhoods
 if (neighborhoodsFilter) {
-    neighborhoodsFilter.setAttribute('role', 'neighborhoods');
+    neighborhoodsFilter.setAttribute('role', 'application');
     neighborhoodsFilter.setAttribute('aria-label', 'choose neighborhoods');
-    neighborhoodsFilter.setAttribute('aria-label-led', 'filter');
-    neighborhoodsFilter.setAttribute('aria-active-option', 'filter-neighborhoods-select');
 }
 
 // Attribute of cuisines
 if (cuisinesSelect) {
-    cuisinesSelect.setAttribute('role', 'cuisines');
+    cuisinesSelect.setAttribute('role', 'application');
     cuisinesSelect.setAttribute('aria-label', 'choose cuisines');
-    cuisinesSelect.setAttribute('aria-label-led', 'filter');
-    cuisinesSelect.setAttribute('aria-active-option', 'cuisines-select');
 }
 
 // Attribute of restaurant list
 if (restaurantsList) {
-    restaurantsList.setAttribute('role', 'review list');
+    restaurantsList.setAttribute('role', 'application');
     restaurantsList.setAttribute('aria-label', 'review list');
 }
--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(image);
   image.setAttribute('alt', `${restaurant.name}`);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
